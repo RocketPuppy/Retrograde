22b6a58a3044079036539c25b284263b7449bcc4
Daniel Wilson-Thomas <daniel@mojotech.com>
2020-02-17 17:29:57 -0500

    Spacecraft rendering

--- retrograde.rb
+++ retrograde.rb
@@ -40,11 +40,26 @@ class Card
     end
   end
 
+  def ship_class_to_file(ship_class)
+    if ship_class == 'Fighter'
+      'icons/fighter.svg'
+    elsif ship_class == 'Corvette'
+      'icons/corvette.svg'
+    elsif ship_class == 'Frigate'
+      'icons/frigate.svg'
+    elsif ship_class == 'Cruiser'
+      'icons/cruiser.svg'
+    else
+      throw "Unknown ship class"
+    end
+  end
+
   def ship_class_embed(embed)
-    embed.svg layout: 'ship-class-icon', key: ':fighter:', file: 'icons/fighter.svg'
-    embed.svg layout: 'ship-class-icon', key: ':corvette:', file: 'icons/corvette.svg'
-    embed.svg layout: 'ship-class-icon', key: ':frigate:', file: 'icons/frigate.svg'
-    embed.svg layout: 'ship-class-icon', key: ':cruiser:', file: 'icons/cruiser.svg'
+    ['Fighter', 'Corvette', 'Frigate', 'Cruiser'].each do |ship_class|
+      embed.svg layout: 'ship-class-icon',
+        key: ship_class_to_icon(ship_class),
+        file: ship_class_to_file(ship_class)
+    end
   end
 end
 
@@ -172,13 +187,13 @@ class Assets < Card
       }).join("\n")
     end
     api.text layout: 'orbitals', str: orbital_string, markup: true do |embed|
-        embed.svg layout: 'orbital-icon', key: ':orbital-up:', file: 'icons/orbital-up.svg'
-        embed.svg layout: 'orbital-icon', key: ':orbital-down:', file: 'icons/orbital-down.svg'
+      embed.svg layout: 'orbital-icon', key: ':orbital-up:', file: 'icons/orbital-up.svg'
+      embed.svg layout: 'orbital-icon', key: ':orbital-down:', file: 'icons/orbital-down.svg'
     end
   end
 end
 
-class Spacecraft
+class Spacecraft < Card
   def data
     Squib.csv file: 'card_data/spacecraft.csv'
   end
@@ -188,11 +203,74 @@ class Spacecraft
   end
 
   def layout
-    []
+    ["layouts/spacecraft.yml"]
   end
 
   # Takes the api object defining the Squib DSL and the deck data to render
   def render(api, data)
+    title api, data['name']
+
+    spacecraft_class api, data['class']
+
+    population api, data['population']
+    bombardment api, data['bombardment']
+    construction api, data['construction']
+    range api, data['range']
+    accuracy api, data['accuracy']
+    armor api, data['armor']
+    delta_vee api, data['delta-vee']
+
+    abilities api, data['abilities']
+  end
+
+  def title(api, title_data)
+    api.text layout: 'title', str: data['name']
+  end
+
+  def spacecraft_class(api, class_data)
+    api.svg layout: 'spacecraft-class', file: class_data.map { |x| ship_class_to_file x }
+  end
+
+  def population(api, pop_data)
+    api.text layout: 'population', str: pop_data.map { |x| ":population:#{x}" } do |embed|
+      embed.svg layout: 'stat-icon', key: ':population:', file: 'icons/population.svg'
+    end
+  end
+
+  def construction(api, construction_data)
+    api.text layout: 'construction', str: construction_data.map { |x| ":construction:#{x}" } do |embed|
+      embed.svg layout: 'stat-icon', key: ':construction:', file: 'icons/construction.svg'
+    end
+  end
+
+  def bombardment(api, bombardment_data)
+    api.text layout: 'bombardment', str: bombardment_data.map { |x| ":bombardment:\n\r #{x}" } do |embed|
+      embed.svg layout: 'stat-icon', key: ':bombardment:', file: 'icons/bombardment.svg'
+    end
+  end
+
+  def range(api, range_data)
+    api.text layout: 'range', str: range_data.map { |x| ":range: #{x}" } do |embed|
+      embed.svg layout: 'stat-icon', key: ':range:', file: 'icons/range.svg'
+    end
+  end
+
+  def accuracy(api, accuracy_data)
+    api.text layout: 'accuracy', str: accuracy_data.map { |x| ":accuracy: #{x}" } do |embed|
+      embed.svg layout: 'stat-icon', key: ':accuracy:', file: 'icons/accuracy.svg'
+    end
+  end
+
+  def armor(api, armor_data)
+    api.text layout: 'armor', str: armor_data.map { |x| ":armor:\n\r #{x}" } do |embed|
+      embed.svg layout: 'stat-icon', key: ':armor:', file: 'icons/armor.svg'
+    end
+  end
+
+  def delta_vee(api, dv_data)
+    api.text layout: 'delta-vee', str: dv_data.map { |x| ":delta-vee:\n\r #{x}" } do |embed|
+      embed.svg layout: 'stat-icon', key: ':delta-vee:', file: 'icons/delta-vee.svg'
+    end
   end
 end
 
