5574b39bec4e29dcbacb4db21157c5c8d9ca7e14
Daniel Wilson-Thomas <daniel@mojotech.com>
2026-02-06 15:40:44 -0500

    Redesign assets for streamlined resources

--- renderer/asset.rb
+++ renderer/asset.rb
@@ -34,39 +34,36 @@ class Asset < Card
 
     horizon api, index
 
-    costs api, data['settlement'], index
+    costs api, data['command_cost'], index
     combat api, data['infrastructure'], index
-    resources api, data['construction'], data['research'].zip(data['research type']), data['colonization'], data['influence'], data['intel'], data['command'], index
+    resources api, data['construction'], data['research'].zip(data['research type']), data['influence'], data['command'], index
 
-    mass_factor api, data['mass factor'], index
+    mass_factor api, data['mass_factor'], index
 
     abilities api, data['abilities'], index
-
-    homeworld api, data['homeworld'], index
   end
 
   private
 
-  def resources(api, constructions, researches, colonizations, influences, intels, commands, index)
+  def resources(api, constructions, researches, influences, commands, index)
     construction_str = constructions.map { |c| ":construction: #{c}" unless c.nil? }
     research_str = researches.map do |r|
-      case r[1].downcase
+      case (r[1] or "").downcase
       when "search"
         ":research-search: #{r[0]}"
-      when "shuffle"
+      when "scry"
         ":research-shuffle: #{r[0]}"
       when "draw"
         ":research-draw: #{r[0]}"
       when "*"
         ":research-wild: #{r[0]}"
       else
-        ":research-draw: 0"
+        nil
       end
     end
-    colonization_str = colonizations.map { |c| ":colonization: #{c}" unless c.nil? }
+
     influence_str = influences.map { |i| ":influence: #{i}" unless i.nil? }
-    intel_str = intels.map { |i| ":intel: #{i}" unless i.nil? }
-    command_str = commands.map { |c| ":command: #{c}" unless c.nil? }
+    command_str = commands.map { |i| i.nil? ? nil : "#{i}"}
 
     api.text layout: 'construction', str: construction_str, range: index do |embed|
       embed.svg layout: 'resource-icon', key: ':construction:', file: 'icons/construction.svg'
@@ -77,14 +74,8 @@ class Asset < Card
       embed.svg layout: 'resource-icon', key: ':research-draw:', file: 'icons/research-draw.svg'
       embed.svg layout: 'resource-icon', key: ':research-wild:', file: 'icons/research-wild.svg'
     end
-    api.text layout: 'colonization', str: colonization_str, range: index do |embed|
-      embed.svg layout: 'resource-icon', key: ':colonization:', file: 'icons/population.svg'
-    end
     api.text layout: 'influence', str: influence_str, range: index do |embed|
       embed.svg layout: 'resource-icon', key: ':influence:', file: 'icons/influence.svg'
-    end
-    api.text layout: 'intel', str: intel_str, range: index do |embed|
-      embed.svg layout: 'resource-icon', key: ':intel:', file: 'icons/intel.svg'
     end
     api.text layout: 'command', str: command_str, range: index do |embed|
       embed.svg layout: 'resource-icon', key: ':command:', file: 'icons/command.svg'
@@ -101,27 +92,13 @@ class Asset < Card
     end
   end
 
-  def homeworld(api, homeworld_data, index)
-    homeworld_data = homeworld_data.map do |h|
-      if h == "Y" then
-        ":homeworld:"
-      else
-        nil
-      end
-    end
-
-    api.text layout: 'homeworld', str: homeworld_data, range: index do |embed|
-      embed.svg layout: 'homeworld-icon', key: ':homeworld:', file: 'icons/homeworld.svg'
-    end
-  end
-
-  def costs(api, settlement_costs, index)
-    cost_strings = settlement_costs.map do |cost|
-      ":settlement: #{cost}" unless cost.nil?
+  def costs(api, command_costs, index)
+    cost_strings = command_costs.map do |cost|
+      ":command: #{cost}" unless cost.nil?
     end
 
     api.text layout: 'costs', str: cost_strings, range: index do |embed|
-      embed.svg layout: 'cost-icon', key: ':settlement:', file: 'icons/settlement.svg'
+      embed.svg layout: 'cost-icon', key: ':command:', file: 'icons/command.svg'
     end
   end
 
